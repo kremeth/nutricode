83a1cca05ba41134fde9b6a7fa2f502ada99ae98
Mathieu Kremeth <mathieukremeth@Mathieus-MacBook-Pro.local>
2023-03-29 11:05:54 +0800

    changed ports

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-app.use(express.static(__dirname + '/public'));
+const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-
-
-app.listen(3000, function() {
-    console.log('Server is running');
-})
+app.listen(port, function() {
+  console.log('Server is running on port:', port);
+});
